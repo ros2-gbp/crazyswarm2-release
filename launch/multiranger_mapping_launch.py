6777d246c25193efd094cbdcb2fd7e1ae173da9b
Wolfgang Hoenig <hoenig@tu-berlin.de>
2026-06-08 22:24:57 +0200

    Rebase from 'debian/humble/crazyflie_examples'

--- launch/multiranger_mapping_launch.py
+++ launch/multiranger_mapping_launch.py
@@ -4,7 +4,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -13,30 +12,11 @@ def generate_launch_description():
         [
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    [
-                        os.path.join(
-                            get_package_share_directory('crazyflie'), 'launch'
-                        ),
-                        '/launch.py',
-                    ]
+                    os.path.join(
+                        get_package_share_directory('crazyflie_examples'),
+                        'launch/keyboard_velmux_launch.py',
+                    )
                 ),
-                launch_arguments={
-                    'backend': 'cflib',
-                    'gui': 'false',
-                    'teleop': 'false',
-                    'mocap': 'false',
-                }.items(),
-            ),
-            Node(
-                package='crazyflie',
-                executable='vel_mux.py',
-                name='vel_mux',
-                output='screen',
-                parameters=[
-                    {'hover_height': 0.3},
-                    {'incoming_twist_topic': '/cmd_vel'},
-                    {'robot_prefix': '/cf231'},
-                ],
             ),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
