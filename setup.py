31ab4e490396a9916dadf276133f30d43656d18e
Wolfgang Hoenig <hoenig@tu-berlin.de>
2026-06-08 22:27:22 +0200

    Rebase from 'debian/jazzy/crazyflie_py'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'crazyflie_py'
 
 setup(
     name=package_name,
-    version='1.0.3',
+    version='1.0.4',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -17,7 +17,9 @@ setup(
     maintainer_email='hoenig@tu-berlin.de, kimberleymcguire@gmail.com',
     description='Simple Python interface for Crazyswarm2',
     license='MIT',
-    tests_require=['pytest'],
+    extras_require={
+        'test': ['pytest'],
+    },
     entry_points={
         'console_scripts': [
         ],
