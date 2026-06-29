95bc31fe5917c0ff04956ebdea07bb27df924cb2
Wolfgang Hoenig <hoenig@tu-berlin.de>
2026-06-29 22:12:00 +0200

    Revert "Trimmed the branch to only the crazyflie_py sub directory"
    This reverts commit b35240414bf97633454983e2332ee6362d84698a.

---
+++ crazyflie_server_py/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'crazyflie_server_py'
+
+setup(
+    name=package_name,
+    version='1.0.4',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Wolfgang H\u00f6nig, Kimberly N. McGuire',
+    maintainer_email='hoenig@tu-berlin.de, kimberleymcguire@gmail.com',
+    description='Python Crazyflie server node for Crazyswarm2',
+    license='MIT',
+    extras_require={
+        'test': ['pytest'],
+    },
+    entry_points={
+        'console_scripts': [
+            'crazyflie_server = crazyflie_server_py.crazyflie_server:main',
+        ],
+    },
+)
