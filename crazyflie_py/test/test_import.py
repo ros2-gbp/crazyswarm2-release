28d3901bbf87419b0a8eaaae70d2df99d530ce70
Wolfgang Hoenig <hoenig@tu-berlin.de>
2026-06-29 22:15:54 +0200

    Trimmed the branch to only the crazyflie sub directory

--- crazyflie_py/test/test_import.py
+++
@@ -1,9 +0,0 @@
-import importlib
-import unittest
-
-
-class TestImport(unittest.TestCase):
-
-    def test_import(self):
-        module = importlib.import_module('crazyflie_py')
-        self.assertIsNotNone(module)
