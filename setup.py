a07abe28f3335af6c3b1eb14797af493e6c60d40
knmcguire <kimberleymcguire@gmail.com>
2026-08-18 21:19:48 +0200

    Rebase from 'release/rolling/crazyflie_py'

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ package_name = 'crazyflie_py'
 
 setup(
     name=package_name,
-    version='1.0.5',
+    version='1.0.6',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'transforms3d'],
     zip_safe=True,
     maintainer='Wolfgang Hönig, Kimberly N. McGuire',
     maintainer_email='hoenig@tu-berlin.de, kimberleymcguire@gmail.com',
